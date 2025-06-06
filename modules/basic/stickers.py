62348c459e1b59c66d7491089a1884a8513efe97
Parvshah-01 <108631149+Parvshah-01@users.noreply.github.com>
2025-06-06 20:23:26 +0530

    Update stickers.py

--- modules/basic/stickers.py
+++ modules/basic/stickers.py
@@ -6,11 +6,13 @@
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
 import contextlib
+import os
 import random, string
 from secrets import token_hex
+import traceback
 
-from telethon import errors
-from telethon.errors.rpcerrorlist import StickersetInvalidError
+from telethon import errors,Button
+from telethon.errors.rpcerrorlist import StickersetInvalidError,ChatSendInlineForbiddenError
 from telethon.tl.functions.messages import GetStickerSetRequest as GetSticker
 from telethon.tl.functions.messages import UploadMediaRequest
 from telethon.tl.functions.stickers import AddStickerToSetRequest as AddSticker
@@ -20,16 +22,14 @@ from telethon.tl.types import (
     InputPeerSelf,
     InputStickerSetEmpty,
 )
-from telethon.errors import StickersetInvalidError, PackShortNameInvalidError
+from telethon.errors import StickersetInvalidError,PeerIdInvalidError,BotInlineDisabledError
 from telethon.tl.types import InputStickerSetItem as SetItem
 from telethon.tl.types import InputStickerSetShortName, User
 from telethon.utils import get_display_name, get_extension, get_input_document
-
+from core.decorators._assistant import callback, in_pattern
 from core.remote import rm
 from utilities.converter import resize_photo_sticker
-
 from .. import LOGS, asst, fetch, udB, ultroid_cmd, get_string
-
 
 async def packExists(packId):
     source = await fetch(f"https://t.me/addstickers/{packId}")
@@ -40,7 +40,6 @@ async def packExists(packId):
         in source
     )
 
-
 async def GetUniquePackName():
     packName = f"{random.choice(string.ascii_lowercase)}{token_hex(random.randint(4, 8))}_by_{asst.me.username}"
     return await GetUniquePackName() if await packExists(packName) else packName
@@ -48,15 +47,13 @@ async def GetUniquePackName():
 
 # TODO: simplify if possible
 
-
 def getName(sender, packType: str):
     title = f"{get_display_name(sender)}'s Kang Pack"
     if packType != "static":
         title += f" ({packType.capitalize()})"
     return title
 
-
-async def AddToNewPack(packType, file, emoji, sender_id, title: str):
+async def AddToNewPack(file, emoji, sender_id, title: str):
     sn = await GetUniquePackName()
     return await asst(
         CreateStickerSetRequest(
@@ -68,6 +65,15 @@ async def AddToNewPack(packType, file, emoji, sender_id, title: str):
         )
     )
 
+async def inline_query_fallback(ult):
+    try:
+        result = await ult.client.inline_query(asst.me.username, "startbot")
+        if result:
+            await result[0].click(ult.chat_id, hide_via=True)
+    except (BotInlineDisabledError, ChatSendInlineForbiddenError):
+        await ult.eor(
+            "❌ Inline mode is disabled in this chat. Please enable inline queries for the bot or use this in a group/channel that allows it."
+        )
 
 @ultroid_cmd(pattern="kang", manager=True)
 async def kang_func(ult):
@@ -76,6 +82,7 @@ async def kang_func(ult):
     sender = await ult.get_sender()
     if not isinstance(sender, User):
         return
+    sender_id = sender.id
     if not ult.is_reply:
         return await ult.eor("`Reply to a message..`", time=5)
     try:
@@ -103,10 +110,15 @@ async def kang_func(ult):
         if type_ or not inPack:
             dl = await reply.download_media()
     elif reply.photo:
-        dl = await reply.download_media()
+        dl = await reply.download_media()        
         name = "sticker.webp"
         image = resize_photo_sticker(dl)
-        image.save(name, "WEBP")
+        image.save(name, "WEBP") 
+        try:
+            os.remove(dl)
+        except:
+            pass                   
+        dl = name  
     elif reply.text:
         with rm.get("quotly", helper=True, dispose=True) as quotly:
             dl = await quotly.create_quotly(reply)
@@ -117,38 +129,52 @@ async def kang_func(ult):
     if dl:
         upl = await asst.upload_file(dl)
         file = get_input_document(await asst(UploadMediaRequest(InputPeerSelf(), upl)))
+        try:
+            os.remove(dl)
+        except:
+            pass
     get_ = udB.get_key("STICKERS") or {}
     title = getName(sender, type_)
-    if not get_.get(ult.sender_id) or not get_.get(ult.sender_id, {}).get(type_):
+    if not get_.get(sender_id) or not get_.get(sender_id, {}).get(type_):
         try:
-            pack = await AddToNewPack(type_, file, emoji, sender.id, title)
+            pack = await AddToNewPack(file, emoji, sender.id, title)
+        except (ValueError, PeerIdInvalidError) as e:
+            await inline_query_fallback(ult)
         except Exception as er:
             return await ult.eor(str(er))
         sn = pack.set.short_name
-        if not get_.get(ult.sender_id):
-            get_.update({ult.sender_id: {type_: [sn]}})
+        if not get_.get(sender_id):
+            get_.update({sender_id: {type_: [sn]}})
         else:
-            get_[ult.sender_id].update({type_: [sn]})
+            get_[sender_id].update({type_: [sn]})
         udB.set_key("STICKERS", get_)
         return await ult.edit(
             f"**Kanged Successfully!\nEmoji :** {emoji}\n**Link :** [Click Here](https://t.me/addstickers/{sn})",
             link_preview=False
         )
-    name = get_[ult.sender_id][type_][-1]
+    name = get_[sender_id][type_][-1]
     try:
         await asst(GetSticker(InputStickerSetShortName(name), hash=0))
     except StickersetInvalidError:
-        get_[ult.sender_id][type_].remove(name)
+        get_[sender_id][type_].remove(name)
     try:
         await asst(
             AddSticker(InputStickerSetShortName(name), SetItem(file, emoji=emoji))
         )
     except (errors.StickerpackStickersTooMuchError, errors.StickersTooMuchError):
         try:
-            pack = await AddToNewPack(type_, file, emoji, sender.id, title)
+            pack = await AddToNewPack(file, emoji, sender.id, title)
+            sn = pack.set.short_name
+        except (ValueError, PeerIdInvalidError) as e:
+            try:
+                await inline_query_fallback(ult)
+            except ChatSendInlineForbiddenError:
+                await ult.eor("❌ Inline mode is disabled in this chat. Please enable inline queries for the bot or use this in a group/channel that allows it.")
+            return
+        
         except Exception as er:
             return await ult.eor(str(er))
-        get_[ult.sender_id][type_].append(pack.set.short_name)
+        get_[sender_id][type_].append(pack.set.short_name)
         udB.set_key("STICKERS", get_)
         return await ult.edit(
             f"**Created New Kang Pack!\nEmoji :** {emoji}\n**Link :** [Click Here](https://t.me/addstickers/{sn})",
@@ -161,7 +187,6 @@ async def kang_func(ult):
         f"Sticker Added to Pack Successfully\n**Link :** [Click Here](https://t.me/addstickers/{name})",
         link_preview=False
     )
-
 
 @ultroid_cmd(pattern="listpack", manager=True)
 async def do_magic(ult):
@@ -184,3 +209,19 @@ async def do_magic(ult):
                     ul[type_].remove(_)
             udB.set_key("STICKERS", ko)
     await ult.reply(msg)
+
+
+# --------------- Handle users who haven't started the assistant bot ---------------
+@in_pattern("startbot")
+async def start_bot(event):
+        result = await event.builder.article(
+            title="Start Assistant Bot to Continue",
+            text=(
+                "To create or manage your sticker pack, you need to start the assistant bot first.\n\n"
+                "Click the button below to start it."
+            ),
+            buttons=[
+                [Button.url("Start Bot", f"https://t.me/{asst.me.username}")]
+            ]
+        )
+        await event.answer([result], private=True, cache_time=0, gallery=False)
